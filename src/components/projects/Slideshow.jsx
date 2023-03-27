8535d50db574f6753a1ec6afffd90b38c5a7cbc9
James Kelly <jk377y@gmail.com>
2023-03-27 01:40:32 -0500

    built component folders and jsx files

---
+++ src/components/projects/Slideshow.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Slideshow = () => {
+  return (
+    <div>Slideshow</div>
+  )
+}
+
+export default Slideshow
---
+++ src/components/resume/Resume.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import CV from '../../assets/pdf/resume.pdf'
+
+
+const Resume = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default Resume
